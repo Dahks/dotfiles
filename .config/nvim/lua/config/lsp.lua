42e40464abe416dc134772c9eedaf72e66d4b4b5
dagur-thoris <dagurtoris@gmail.com>
2026-01-26 23:09:44 +0000

    tweak python lsp config

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -5,41 +5,16 @@ vim.lsp.enable('json_ls')
 vim.lsp.enable('lua_ls')
 vim.lsp.enable('ts_ls')
 vim.lsp.enable('twd_ls')
-vim.lsp.enable("clangd")
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'python',
-  callback = function(args)
-    local root_markers = {
-        'pyrefly.toml',
-        'pyproject.toml',
-        'setup.py',
-        'requirements.txt',
-        '.git',
-    }
-
-    local root_dir = vim.fs.root(args.buf, root_markers)
-
-    if root_dir then
-      vim.lsp.start({
-        name = 'pyrefly',
-        cmd = { 'pyrefly', 'lsp' },
-        root_dir = root_dir,
-        capabilities = require('blink.cmp').get_lsp_capabilities(),
-      })
-    end
-  end,
-})
+vim.lsp.enable('clangd')
+vim.lsp.enable('pyrefly')
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(args)
     -- Enable 'gd' to jump to definition
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = args.buf, desc = "Go to Definition" })
-    
     -- 'gr' for references (where is this function used?)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = args.buf, desc = "Go to References" })
-    
     -- 'K' for hover documentation
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf, desc = "Hover Documentation" })
   end,
